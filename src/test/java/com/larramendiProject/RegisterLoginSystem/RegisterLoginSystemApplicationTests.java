e1938ead2183c32fb84cd44419040401b1298cef
Gabriel Larramendi <larramendiga@gmail.com>
2023-07-18 22:44:38 -0300

    firts commit

---
+++ src/test/java/com/larramendiProject/RegisterLoginSystem/RegisterLoginSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larramendiProject.RegisterLoginSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLoginSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
